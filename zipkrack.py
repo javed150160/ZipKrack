2c301f88c48f0923ca09dc5d8bc7960eba1c6214
javed150160 <165566071+javed150160@users.noreply.github.com>
2024-09-04 18:24:13 +0500

    Update zipkrack.py

--- zipkrack.py
+++ zipkrack.py
@@ -1,9 +1,9 @@
 #!/usr/bin/env python3
 
-Tool_Name = "ZipKrack"
+Tool_Name = "JAVED-DL"
 Version = "1.2.0"
 """
-Author: Sajjad
+Author: JAVED-DL
 GitHub: https://github.com/Cyber-Anonymous
 """
 
@@ -11,7 +11,6 @@ from zipfile import ZipFile
 import sys
 import time
 import argparse
-
 
 parser = argparse.ArgumentParser(description="A tool for cracking passwords of encrypted ZIP files using a provided wordlist.")
 parser.add_argument("-z", "--zipfile", type=str, help="Specify the ZIP file.")
@@ -22,110 +21,98 @@ parser.add_argument("--version", action="version", version="{} {}".format(Tool_N
 args = parser.parse_args()
 
 def banner():
-
     print("""\033[1;96m
-  ______       _  __               _
- |__  (_)_ __ | |/ /_ __ __ _  ___| | __
-   / /| | '_ \| ' /| '__/ _` |/ __| |/ /
-  / /_| | |_) | . \| | | (_| | (__|   <
- /____|_| .__/|_|\_\_|  \__,_|\___|_|\_\\
-        |_|              Coded by Sajjad
+     ___  _    ____________
+     |   | |  / / ____/ __ \\
+  _  | | | | / / __/ / / / /
+ | |_| | |/ / /___/ /_/ /
+ |_____|___/_____/_____/
+   CODED BY JAVED KHAN
 \033[0m""")
 
-
-
 if (bool(args.zipfile) == True):
-	action = args.zipfile
-	try:
-		zip = ZipFile(action, "r")
-	except Exception as error:
-		print("\n\033[0;91m[ERROR] {}\033[0;0m\n".format(error))
-
+    action = args.zipfile
+    try:
+        zip = ZipFile(action, "r")
+    except Exception as error:
+        print("\n\033[0;91m[ERROR] {}\033[0;0m\n".format(error))
 
 elif (bool(args.zipfile) == False):
-	banner()
-	while(True):
-		action=input("Zip file > ")
-		if(action=="quit" or action=="exit"):
-			sys.exit()
-		else:
-			pass
-		try:
-			zip = ZipFile(action,"r")
-			break
+    banner()
+    while(True):
+        action = input("Zip file > ")
+        if(action == "quit" or action == "exit"):
+            sys.exit()
+        else:
+            pass
+        try:
+            zip = ZipFile(action, "r")
+            break
 
-		except:
-			if(action==""):
-				pass
-			else:
-				print("\n\033[0;91m[ERROR] Unable to locate the zip file.\033[0;0m\n")
+        except:
+            if(action == ""):
+                pass
+            else:
+                print("\n\033[0;91m[ERROR] Unable to locate the zip file.\033[0;0m\n")
 
 else:
-	pass
-
+    pass
 
 if(bool(args.wordlist) == True):
-	list = args.wordlist
-	try:
-		wordlist = open(list, "r", encoding="utf-8", errors="replace")
+    list = args.wordlist
+    try:
+        wordlist = open(list, "r", encoding="utf-8", errors="replace")
 
-	except Exception as error:
-		print("\n\033[0;91m[ERROR] {}\033[0;0m\n".format(error))
+    except Exception as error:
+        print("\n\033[0;91m[ERROR] {}\033[0;0m\n".format(error))
 
 elif(bool(args.wordlist) == False):
-	while(True):
-		list=input("Password list > ")
-		if(list=="quit" or list=="exit"):
-			sys.exit()
-		try:
-			wordlist = open(list,"r")
-			break
-		except:
-			if(list == ""):
-				pass
-			else:
-				print("\n\033[0;91m[ERROR] Unable to locate the password list.\033[0;0m\n")
+    while(True):
+        list = input("Password list > ")
+        if(list == "quit" or list == "exit"):
+            sys.exit()
+        try:
+            wordlist = open(list, "r")
+            break
+        except:
+            if(list == ""):
+                pass
+            else:
+                print("\n\033[0;91m[ERROR] Unable to locate the password list.\033[0;0m\n")
 
 else:
-	pass
-	
+    pass
 
-	
+
 print("\n")
 
-condition=0
+condition = 0
 attempts = 0
 start = time.time()
 for password in wordlist:
-	password = password.strip()
-	sys.stdout.write("\r\033[K")
-	sys.stdout.write("\rAttemping password: {}".format(password))
-	sys.stdout.flush()
-	try:
-		zip.extractall(path=args.output,pwd=password.encode("utf-8"))
-		print("\n\n\033[1;92mFound password: {}\033[0;0m".format(password))
-		timestamp = time.strftime("%y-%m-%d_%H-%M-%S", time.localtime())
-		file_name = "{}.txt".format(timestamp)
-		with open(file_name,"w") as pass_file:
-			pass_file.write(password)
-			print("\nPassword stored in {}.".format(file_name))
-			
-		condition+=1
-		break
-	except:
-		pass
-	attempts +=1
+    password = password.strip()
+    sys.stdout.write("\r\033[K")
+    sys.stdout.write("\rAttemping password: {}".format(password))
+    sys.stdout.flush()
+    try:
+        zip.extractall(path=args.output, pwd=password.encode("utf-8"))
+        print("\n\n\033[1;92mFound password: {}\033[0;0m".format(password))
+        timestamp = time.strftime("%y-%m-%d_%H-%M-%S", time.localtime())
+        file_name = "{}.txt".format(timestamp)
+        with open(file_name, "w") as pass_file:
+            pass_file.write(password)
+            print("\nPassword stored in {}.".format(file_name))
+
+        condition += 1
+        break
+    except:
+        pass
+    attempts += 1
 
 zip.close()
 wordlist.close()
 
-if(condition==0):
-	print("\nNo valid password found.")
+if(condition == 0):
+    print("\nNo valid password found.")
 else:
-	pass
-
-elapsed = time.time() - start
-elapsed_time = time.strftime("%H:%M:%S", time.gmtime(elapsed))
-
-print("Elapsed Time: {}".format(elapsed_time))
-print("Attempted a total of {} passwords.".format(attempts))
+    pass
